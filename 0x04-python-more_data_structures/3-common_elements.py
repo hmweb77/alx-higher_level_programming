991fe5a7317f560454496d7d46f64b6e38c24ede
hmweb77 <oussamahamdani1@gmail.com>
2023-09-15 18:22:15 +0100

    done

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    return set_1 & set_2
+# The intersection set of A and B, denoted “A ∩ B” (read “A inter B”)
+# We can use set_1.intersection(set_2) too.
